9e2aec863ab5da19c69cab140f86b97da5ef9df1
Yaroslav Nosik <nosik.yaroslav@gmail.com>
2020-05-16 22:13:34 +0300

    usage of RingView

--- DesignCode/RingView.swift
+++ DesignCode/RingView.swift
@@ -15,6 +15,8 @@ struct RingView: View {
     var height: CGFloat = 88
     var percent: CGFloat = 88
     
+    @Binding var show: Bool
+    
     var body: some View {
         let multiplier = width / 44
         let progress = 1 - percent / 100
@@ -24,7 +26,7 @@ struct RingView: View {
                 .stroke(Color.black.opacity(0.1), style: StrokeStyle(lineWidth: 5 * multiplier))
                 .frame(width: width, height: height)
             Circle()
-                .trim(from: progress, to: 1)
+                .trim(from: show ? progress : 1, to: 1)
                 .stroke(
                     LinearGradient(gradient: Gradient(colors: [Color(color1), Color(color2)]), startPoint: .topTrailing, endPoint: .bottomLeading),
                     style: StrokeStyle(lineWidth: 5 * multiplier,
@@ -42,12 +44,15 @@ struct RingView: View {
             Text("\(Int(percent))%")
                 .font(.system(size: 14 * multiplier))
                 .fontWeight(.bold)
+                .onTapGesture {
+                    self.show.toggle()
+            }
         }
     }
 }
 
 struct RingView_Previews: PreviewProvider {
     static var previews: some View {
-        RingView()
+        RingView(show: .constant(true))
     }
 }
